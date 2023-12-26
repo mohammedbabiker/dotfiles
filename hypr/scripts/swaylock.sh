0af4778c6d50bdb70d651be5547038790def6fd9
mohammedbabiker <mobabikerbabai20@gmail.com>
2023-12-26 11:20:14 +0300

    volume key bind

--- hypr/scripts/swaylock.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-pkill xautolock
-
-xautolock -time 10 -locker "swaylock -i ~/.cache/current.png" -notify 30 -notifier "notify-send 'Screen will be locked soon.' 'Locking screen in 30 seconds'"
-
