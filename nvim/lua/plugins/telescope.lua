aa070ad27eea6c8580fb5ab8860ad75e7777d1ac
mohammedbabiker <mobabikerbabai20@gmail.com>
2024-01-01 22:17:15 +0300

    Install lang server to nvim

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,5 +1,6 @@
 
 return {
+  {
     'nvim-telescope/telescope.nvim', tag = '0.1.5',
     dependencies = { 'nvim-lua/plenary.nvim' },
     config = function()
@@ -7,4 +8,19 @@ return {
         vim.keymap.set('n', '<C-p>', builtin.find_files, {})
         vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
     end
+  },
+  {
+    "nvim-telescope/telescope-ui-select.nvim",
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          ["ui-select"] = {
+            require("telescope.themes").get_dropdown {
+            }
+          }
+        }
+      })
+      require("telescope").load_extension("ui-select")
+    end
+  },
 }
