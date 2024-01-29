8da48e60581cb076631b59817decca84fb9b56ac
mohammedbabiker <mobabikerbabai20@gmail.com>
2024-01-29 23:04:11 +0300

    Remove none-use scripts

--- hypr/scripts/lockscreentime.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-source ~/.config/.settings/swayidle.sh
-if [ -f "/usr/bin/swayidle" ]; then
-    echo "swayidle is installed."
-    swayidle -w timeout $timeswaylock 'swaylock -f' timeout $timeoff 'hyprctl dispatch dpms off' resume 'hyprctl dispatch dpms on'
-else
-    echo "swayidle not installed."
-fi;
