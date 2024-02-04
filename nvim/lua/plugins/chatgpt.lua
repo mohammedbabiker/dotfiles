47f7d842ecf21d4d40f1eb58f17ecb4aa4b5083d
mohammedbabiker <mobabikerbabai20@gmail.com>
2024-02-04 21:59:52 +0300

    Store chatgpt api with pass
    Api added

--- nvim/lua/plugins/chatgpt.lua
+++ nvim/lua/plugins/chatgpt.lua
@@ -1,12 +1,16 @@
 return {
-  "jackMort/ChatGPT.nvim",
+  {
+    "jackMort/ChatGPT.nvim",
     event = "VeryLazy",
-    config = function()
-      require("chatgpt").setup()
-    end,
     dependencies = {
       "MunifTanjim/nui.nvim",
       "nvim-lua/plenary.nvim",
       "nvim-telescope/telescope.nvim"
-    }
+    },
+    config = function()
+      require("chatgpt").setup({
+        api_key_cmd = "pass show api/chatgpt",
+      })
+    end,
+  }
 }
