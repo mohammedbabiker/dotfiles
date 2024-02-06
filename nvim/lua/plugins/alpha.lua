8fdf3d8d398e951feb3838623cf886a42df93b95
mohammedbabiker <mobabikerbabai20@gmail.com>
2024-02-06 18:10:36 +0300

    Update dashboard button
    with formmating

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -1,48 +1,61 @@
 return {
-	"goolord/alpha-nvim",
+  "goolord/alpha-nvim",
   event = "VimEnter", -- load plugin after all configuration is set
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
 
-	config = function()
-		local alpha = require("alpha")
-		local dashboard = require("alpha.themes.dashboard")
+  config = function()
+    local alpha = require("alpha")
+    local dashboard = require("alpha.themes.dashboard")
 
-		dashboard.section.header.val = {
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                     ]],
-			[[       ████ ██████           █████      ██                     ]],
-			[[      ███████████             █████                             ]],
-			[[      █████████ ███████████████████ ███   ███████████   ]],
-			[[     █████████  ███    █████████████ █████ ██████████████   ]],
-			[[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
-			[[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
-			[[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-			[[                                                                       ]],
-		}
+    dashboard.section.header.val = {
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                     ]],
+      [[       ████ ██████           █████      ██                     ]],
+      [[      ███████████             █████                             ]],
+      [[      █████████ ███████████████████ ███   ███████████   ]],
+      [[     █████████  ███    █████████████ █████ ██████████████   ]],
+      [[    █████████ ██████████ █████████ █████ █████ ████ █████   ]],
+      [[  ███████████ ███    ███ █████████ █████ █████ ████ █████  ]],
+      [[ ██████  █████████████████████ ████ █████ █████ ████ ██████ ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+      [[                                                                       ]],
+    }
 
-		_Gopts = {
-			position = "center",
-			hl = "Type",
-			-- wrap = "overflow";
-		}
+    _Gopts = {
+      position = "center",
+      hl = "Type",
+      -- wrap = "overflow";
+    }
 
-		local function footer()
-			return "Mohammed Babiker Babai"
-		end
+    -- Set menu
+    dashboard.section.buttons.val = {
+      -- dashboard.button("SPC j", "󰈚   Restore Session", ":SessionRestore<cr>"),
+      dashboard.button("e", "   New file", ":ene <BAR> startinsert <CR>"),
+      dashboard.button("f", "   Find file", ":cd $HOME/mohmd-Hyprland-Dotfile | Telescope find_files<CR>"),
+      dashboard.button("g", "󰱼   Find word", ":Telescope live_grep<CR>"),
+      dashboard.button("r", "   Recent", ":Telescope oldfiles<CR>"),
+      dashboard.button("c", "   Config", ":e $MYVIMRC <CR>"),
+      dashboard.button("m", "󱌣   Mason", ":Mason<CR>"),
+      dashboard.button("u", "󰂖   Update plugins", "<cmd>lua require('lazy').sync()<CR>"),
+      dashboard.button("q", "   Quit NVIM", ":qa<CR>"),
+    }
 
-		dashboard.section.footer.val = footer()
+    local function footer()
+      return "Mohammed Babiker Babai"
+    end
 
-		dashboard.opts.opts.noautocmd = true
-		alpha.setup(dashboard.opts)
-	end,
+    dashboard.section.footer.val = footer()
+
+    dashboard.opts.opts.noautocmd = true
+    alpha.setup(dashboard.opts)
+  end,
 }
