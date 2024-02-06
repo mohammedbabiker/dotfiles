100b0d150cf131eb8c6dd1bee95f8bd32547f785
mohammedbabiker <mobabikerbabai20@gmail.com>
2024-02-06 16:36:52 +0300

    Add css languager server for snipets

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -20,17 +20,24 @@ return {
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-      local lspconfig = require("lspconfig")
-      lspconfig.tsserver.setup({
+      local lspconfig = require("lspconfig") -- language server
+      lspconfig.tsserver.setup({             -- javascript
         capabilites = capabilities,
       })
-      lspconfig.html.setup({
+      lspconfig.html.setup({                 -- html
         capabilites = capabilities,
       })
-      lspconfig.lua_ls.setup({
+      lspconfig.lua_ls.setup({               -- lua
+        capabilites = capabilities,
+      })
+      lspconfig.cssls.setup({                -- css
+        capabilites = capabilities,
+      })
+      lspconfig.tailwindcss.setup({
         capabilites = capabilities,
       })
 
+      vim.keymap.set("n", "I", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
       vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
