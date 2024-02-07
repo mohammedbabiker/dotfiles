4174120eecb28b92b07f1fbb62caa3f34ff45b41
mohammedbabiker <mobabikerbabai20@gmail.com>
2024-02-07 22:40:00 +0300

    Set numbers and cursorline
    Nvim configure

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -3,3 +3,6 @@ vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
+vim.cmd("set cursorline")
+vim.cmd("set number")
+--vim.cmd("set relativenumber")
