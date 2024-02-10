a49c4f8de7df1d3b0d1ac5da64c94ad956ed3075
mohammedbabiker <mobabikerbabai20@gmail.com>
2024-02-10 18:09:50 +0300

    Test debug go language

--- nvim/lua/debagging-test.go
+++ nvim/lua/debagging-test.go
@@ -3,6 +3,20 @@ package main
 import "fmt"
 
 func main() {
-  fmt.PrintIn("hello world")
+    var array = []int{1, 2, 3, 4, 5, 6}
+    for _, num := range array {
+        if isEven(num) {
+            fmt.Printf("number %d is even\n", num)
+        } else {
+            fmt.Printf("number %d is odd\n", num)
+        }
+    }
 }
 
+func isEven(num int) bool {
+    res := num % 2
+    if res == 1 {
+        return true
+    }
+    return false
+}
