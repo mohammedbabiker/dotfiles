f901b861e341ed041b2b54a26b0534033fafe3e6
mohammedbabiker <mobabikerbabai20@gmail.com>
2024-02-12 14:42:52 +0300

    Hide cmd line when exec command

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -9,8 +9,8 @@ return {
     config = function()
       require("gitsigns").setup()
 
-      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
-      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+      vim.keymap.set("n", "<leader>gp", "<CMD>Gitsigns preview_hunk<CR>", {})
+      vim.keymap.set("n", "<leader>gt", "<CMD>Gitsigns toggle_current_line_blame<CR>", {})
     end
   }
 }
