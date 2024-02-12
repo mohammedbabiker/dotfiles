37a0a9ae8b2b310cb5f0b356517d021c20d88a93
mohammedbabiker <mobabikerbabai20@gmail.com>
2024-02-12 21:09:48 +0300

    Update truble keymap

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -3,10 +3,9 @@ return {
   dependencies = { "nvim-tree/nvim-web-devicons" },
   cmd = { "TroubleToggle", "Trouble" },
   keys = {
-    { "<leader>t", desc = "Trouble" },
-    { "<leader>w", "<CMD>TroubleToggle workspace_diagnostics<CR>" },
-    { "<leader>d", "<CMD>TroubleToggle document_diagnostics<CR>" },
-    { "<leader>f", "<CMD>TroubleToggle quickfix<CR>" },
+    { "<leader>tw", "<CMD>TroubleToggle workspace_diagnostics<CR>" },
+    { "<leader>td", "<CMD>TroubleToggle document_diagnostics<CR>" },
+    { "<leader>tf", "<CMD>TroubleToggle quickfix<CR>" },
   },
   otps = {
     use_diagnostic_signs = true,
