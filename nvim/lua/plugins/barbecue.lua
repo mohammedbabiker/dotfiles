d48b1f25367044713cd1b5f303d7d562e21f12dd
mohammedbabiker <mobabikerbabai20@gmail.com>
2024-02-15 18:18:36 +0300

    disable plugin

--- nvim/lua/plugins/barbecue.lua
+++ nvim/lua/plugins/barbecue.lua
@@ -1,13 +1,13 @@
 return {
-  "utilyre/barbecue.nvim",
-  event = "VeryLazy",
-  name = "barbecue",
-  version = "*",
-  dependencies = {
-    "SmiteshP/nvim-navic",
-    "nvim-tree/nvim-web-devicons", -- optional dependency
-  },
-  opts = {
-    -- configurations go here
-  },
+  -- "utilyre/barbecue.nvim",
+  -- event = "VeryLazy",
+  -- name = "barbecue",
+  -- version = "*",
+  -- dependencies = {
+  --   "SmiteshP/nvim-navic",
+  --   "nvim-tree/nvim-web-devicons", -- optional dependency
+  -- },
+  -- opts = {
+  --   -- configurations go here
+  -- },
 }
