b5732abba5d7f2b8d84dbe71a865f33e45d9429b
mohammedbabiker <mobabikerbabai20@gmail.com>
2024-02-26 00:24:00 +0300

    Update vim options

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -1,8 +1,8 @@
+vim.g.mapleader = " "
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.g.mapleader = " "
 vim.cmd("set cursorline")
 vim.cmd("set number")
 vim.cmd("set relativenumber")
@@ -13,4 +13,6 @@ vim.cmd("set ignorecase") -- ignore capital late on search
 vim.cmd("set spell")
 vim.cmd("set autoindent")
 vim.cmd("set smartcase")
+vim.cmd("set scrolloff=5")
+vim.cmd("set undofile")
 --vim.cmd("set spelllang=en_us")
