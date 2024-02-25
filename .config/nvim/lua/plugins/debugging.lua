2e0c3ef62ba3baeedfb123c6fb013c947a52b8e5
mohammedbabiker <mobabikerbabai20@gmail.com>
2024-02-25 04:15:41 +0300

    Add keymapping

--- .config/nvim/lua/plugins/debugging.lua
+++ .config/nvim/lua/plugins/debugging.lua
@@ -2,7 +2,9 @@ return {
   "mfussenegger/nvim-dap",
   keys = { -- load only when click on keys
     { "<leader>dt", "<CMD>DapToggleBreakpoint<CR>" },
-    { "<leader>dc", "<CMD>DapContinue<CR>" }
+    { "<leader>dc", "<CMD>DapContinue<CR>" },
+    { "<F10>",      "<CMD>DapStepOver<CR>" },
+    { "<F12>",      "<CMD>DapStepOut<CR>" },
   },
   dependencies = {
     "rcarriga/nvim-dap-ui",
