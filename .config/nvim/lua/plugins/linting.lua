4c8628b8baf317402e24d5c7331051a1f7f92d7a
mohammedbabiker <mobabikerbabai20@gmail.com>
2024-02-29 22:26:37 +0300

    Enable linting

--- .config/nvim/lua/plugins/linting.lua
+++ .config/nvim/lua/plugins/linting.lua
@@ -16,14 +16,14 @@ return {
 			python = { "flake8" },
 		}
 
-		-- local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
-		--
-		-- vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-		-- 	group = lint_augroup,
-		-- 	callback = function()
-		-- 		lint.try_lint()
-		-- 	end,
-		-- })
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
 
 		vim.keymap.set("n", "<leader>ln", function()
 			lint.try_lint()
