4a5d7aecb78148e05e6a00b84940c4dda5149f3a
mohammedbabiker <mobabikerbabai20@gmail.com>
2024-03-01 03:28:58 +0300

    Set higlight on yank autocmd

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -29,3 +29,13 @@ local keymap = vim.keymap
 keymap.set("i", "jk", "<ESC>") -- jk to escape
 keymap.set("n", "<leader>nh", ":nohl<CR>") -- clear highlight
 keymap.set("n", "x", '"-x"') -- dont yank on x
+
+-- show yand highlight
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+	pattern = "*",
+	desc = "Highlight selection on yank",
+	callback = function()
+		vim.highlight.on_yank({ timeout = 200, visual = true })
+	end,
+})
