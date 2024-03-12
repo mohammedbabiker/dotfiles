90538d1893b95cd49eb8e2f038af5378e89a7992
Mohammed Babiker <mobabikerbabai20@gmail.com>
2024-03-12 19:15:36 +0300

    Update linting for javascript

--- config/nvim/lua/plugins/linting.lua
+++ config/nvim/lua/plugins/linting.lua
@@ -8,7 +8,7 @@ return {
 		local lint = require("lint")
 
 		lint.linters_by_ft = {
-			-- javascript = { "eslint_d" },
+			javascript = { "eslint_d" },
 			typescript = { "eslint_d" },
 			typescriptreact = { "eslint_d" },
 			javascriptreact = { "eslint_d" },
