b308adf90df9930db1f98c1a115b72e4d6a80e09
Mohammed Babiker <mobabikerbabai20@gmail.com>
2024-03-18 20:20:33 +0300

    Update keybinding
    telescope keybinds

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -26,11 +26,10 @@ return {
 			})
 
 			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<C-p>", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>lg", builtin.live_grep, {}) -- cd $HOME/dotfiles |
-			vim.keymap.set("n", "<leader>of", builtin.oldfiles, {})
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, {}) -- cd $HOME/dotfiles |
+			vim.keymap.set("n", "<leader>fo", builtin.oldfiles, {})
 			vim.keymap.set("n", "<leader>fs", builtin.grep_string, {})
-			vim.keymap.set("n", "<leader>fg", builtin.git_files, {})
 
 			require("telescope").load_extension("ui-select")
 		end,
