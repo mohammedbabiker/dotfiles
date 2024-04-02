6471d1b6d77d1a35529271688be42ad9700d7881
Mohammed Babiker <mobabikerbabai20@gmail.com>
2024-04-02 15:43:11 +0300

    Updates
    formatting

--- config/nvim/lua/plugins/tmux.lua
+++ config/nvim/lua/plugins/tmux.lua
@@ -1,11 +1,11 @@
 return {
-	"christoomey/vim-tmux-navigator",
-	-- lazy = false,
-	event = { "BufReadPre", "BufNewFile" },
-	keys = {
-		{ "<c-h>", "<cmd> TmuxNavigateLeft<cr>" },
-		{ "<c-l>", "<cmd> TmuxNavigateRight<cr>" },
-		{ "<c-j>", "<cmd> TmuxNavigateDown<cr>" },
-		{ "<c-k>", "<cmd> TmuxNavigateUp<cr>" },
-	},
+  "christoomey/vim-tmux-navigator",
+  -- lazy = false,
+  event = { "BufReadPre", "BufNewFile" },
+  keys = {
+    { "<c-h>", "<cmd> TmuxNavigateLeft<cr>" },
+    { "<c-l>", "<cmd> TmuxNavigateRight<cr>" },
+    { "<c-j>", "<cmd> TmuxNavigateDown<cr>" },
+    { "<c-k>", "<cmd> TmuxNavigateUp<cr>" },
+  },
 }
