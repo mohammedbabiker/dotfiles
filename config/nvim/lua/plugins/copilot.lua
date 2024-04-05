9a51db2f92bc327ba29c0d623e0c4239005c1a40
Mohammed Babiker <mobabikerbabai20@gmail.com>
2024-04-05 21:35:39 +0300

    Update

--- config/nvim/lua/plugins/copilot.lua
+++ config/nvim/lua/plugins/copilot.lua
@@ -1,18 +1,18 @@
 return {
-	"github/copilot.vim",
-	event = { "BufReadPre", "BufNewFile" },
-	-- altervative
-	-- "Exafunction/codeium.vim",
-	-- event = "BufEnter",
-	-- config = function()
-	-- 	vim.keymap.set("i", "<c-;>", function()
-	-- 		return vim.fn["codeium#CycleCompletions"](1)
-	-- 	end, { expr = true, silent = true })
-	-- 	vim.keymap.set("i", "<c-,>", function()
-	-- 		return vim.fn["codeium#CycleCompletions"](-1)
-	-- 	end, { expr = true, silent = true })
-	-- 	vim.keymap.set("i", "<c-x>", function()
-	-- 		return vim.fn["codeium#Clear"]()
-	-- 	end, { expr = true, silent = true })
-	-- end,
+  "github/copilot.vim",
+  event = { "BufReadPre", "BufNewFile" },
+  -- altervative
+  -- "Exafunction/codeium.vim",
+  -- event = "BufEnter",
+  -- config = function()
+  -- 	vim.keymap.set("i", "<c-;>", function()
+  -- 		return vim.fn["codeium#CycleCompletions"](1)
+  -- 	end, { expr = true, silent = true })
+  -- 	vim.keymap.set("i", "<c-,>", function()
+  -- 		return vim.fn["codeium#CycleCompletions"](-1)
+  -- 	end, { expr = true, silent = true })
+  -- 	vim.keymap.set("i", "<c-x>", function()
+  -- 		return vim.fn["codeium#Clear"]()
+  -- 	end, { expr = true, silent = true })
+  -- end,
 }
