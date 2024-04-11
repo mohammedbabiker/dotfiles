f5ad9259755668dc518edc9a7a04fd41edc131c5
Mohammed Babiker <mobabikerbabai20@gmail.com>
2024-04-11 04:01:02 +0300

    Change indent line

--- config/nvim/lua/plugins/indent-rainbowline.lua
+++ config/nvim/lua/plugins/indent-rainbowline.lua
@@ -1,10 +1,11 @@
 return {
-	{
-		"lukas-reineke/indent-blankline.nvim",
-		event = "BufReadPost",
-		main = "ibl",
-		opts = {
-			enabled = true,
-		},
-	},
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    main = "ibl",
+    opts = {
+      enabled = true,
+      indent = { char = "┊" },
+    },
+  },
 }
