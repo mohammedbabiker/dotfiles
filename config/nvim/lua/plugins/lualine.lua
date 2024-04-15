2d1f7269211a4fdbabaae7634c9fe1d78f4c0f01
Mohammed Babiker <mobabikerbabai20@gmail.com>
2024-04-15 12:57:12 +0300

    extention

--- config/nvim/lua/plugins/lualine.lua
+++ config/nvim/lua/plugins/lualine.lua
@@ -1,29 +1,30 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	event = "VeryLazy", -- load this plugin after critical plugins
-	config = function()
-		require("lualine").setup({
-			options = {
-				theme = "dracula",
-			},
-			sections = {
-				lualine_a = { "filename" },
-				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = {
-					{
-						require("noice").api.statusline.mode.get,
-						cond = require("noice").api.statusline.mode.has,
-					},
-					-- "mode",
-				},
-				lualine_x = {
-					-- "encoding",
-					"fileformat",
-					"filetype",
-				},
-				lualine_y = { "progress" },
-				lualine_z = { "location" },
-			},
-		})
-	end,
+  "nvim-lualine/lualine.nvim",
+  event = "VeryLazy", -- load this plugin after critical plugins
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "dracula",
+      },
+      sections = {
+        lualine_a = { "filename" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = {
+          {
+            require("noice").api.statusline.mode.get,
+            cond = require("noice").api.statusline.mode.has,
+          },
+          -- "mode",
+        },
+        lualine_x = {
+          -- "encoding",
+          "fileformat",
+          "filetype",
+        },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+      extensions = { "fugitive", "lazy", "mason", "nvim-dap-ui" },
+    })
+  end,
 }
