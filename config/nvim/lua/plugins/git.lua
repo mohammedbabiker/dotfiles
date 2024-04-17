63f06084d26a51bcbd1931040fda3bd20854fe5a
Mohammed Babiker <mobabikerbabai20@gmail.com>
2024-04-17 19:19:39 +0300

    Updates

--- config/nvim/lua/plugins/git.lua
+++ config/nvim/lua/plugins/git.lua
@@ -1,18 +1,26 @@
 return {
-	{
-		"tpope/vim-fugitive",
-		event = { "BufReadPre", "BufNewFile" }, -- load the plugin when entering a buffer
-	},
-	{
-		"lewis6991/gitsigns.nvim",
-		event = { "BufReadPre", "BufNewFile" }, -- load the plugin when entering a buffer
-		config = function()
-			require("gitsigns").setup()
+  {
+    "tpope/vim-fugitive",
+    event = { "BufReadPre", "BufNewFile" }, -- load the plugin when entering a buffer
+  },
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" }, -- load the plugin when entering a buffer
+    config = function()
+      require("gitsigns").setup({
+        signs = {
+          add = { text = '+' },
+          change = { text = '~' },
+          delete = { text = '_' },
+          topdelete = { text = '‾' },
+          changedelete = { text = '~' },
+        },
+        current_line_blame = false, -- show by <leader>gt
+      })
 
-			vim.keymap.set("n", "<leader>gp", "<CMD>Gitsigns preview_hunk<CR>", {})
-			vim.keymap.set("n", "<leader>gt", "<CMD>Gitsigns toggle_current_line_blame<CR>", {})
+      vim.keymap.set("n", "<leader>gp", "<CMD>Gitsigns preview_hunk<CR>", {})
+      vim.keymap.set("n", "<leader>gt", "<CMD>Gitsigns toggle_current_line_blame<CR>", {})
       vim.keymap.set("n", "<leader>gf", "<CMD>Gitsigns diffthis<CR>", {})
-		end,
-	},
+    end,
+  },
 }
-
