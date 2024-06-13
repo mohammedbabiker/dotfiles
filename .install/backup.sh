136e3675c5f58d1bb2c69a2888ff03990ebc0810
Mohammed Babiker <mobabikerbabai20@gmail.com>
2024-06-13 11:43:47 +0300

    Update backup script

--- .install/backup.sh
+++ .install/backup.sh
@@ -1,15 +1,11 @@
 # Backup existing dotfiles
 
 datets=$(date '+%Y-%m\%d-%H:%M:%S')
-if [ -d ~/dotfiles ] || [ -f ~/.zshrc ]; then
+if [ -d ~/.dotfiles ] ; then
   figlet "backup"
-  if [ -d ~/dotfiles ]; then
+  if [ -d ~/.dotfiles ]; then
         echo "The script has detected an existing dotfiles folder and will try to create a backup into the folder:"
         echo "~/dotfiles-backup/$datets"
-    fi
-    if [ ! -L ~/.zshrc ] && [ -f ~/.zshrc ]; then
-        echo "The script has detected an existing .zshrc file and will try to create a backup to:" 
-        echo "~/dotfiles-backup/$datets/.zshrc-old"
     fi
     if gum confirm "Do you want to create a backup?" ;then
         if [ ! -d ~/dotfiles-backup ]; then
@@ -20,13 +16,9 @@ if [ -d ~/dotfiles ] || [ -f ~/.zshrc ]; then
             mkdir ~/dotfiles-backup/$datets
             echo "~/dotfiles-backup/$datets created"
         fi
-        if [ -d ~/dotfiles ]; then
-            rsync -a ~/dotfiles/ ~/dotfiles-backup/$datets/
+        if [ -d ~/.dotfiles ]; then
+            rsync -a ~/.dotfiles/ ~/dotfiles-backup/$datets/
             echo "Backup of your current files in ~/dotfiles-backup/$datets created."
-        fi
-        if [ -f ~/.zshrc ]; then
-            cp ~/.zshrc ~/dotfiles-backup/$datets/.zshrc-old
-            echo "Existing .zshrc file found in homefolder. .zshrc-old created"
         fi
     elif [ $? -eq 130 ]; then
         exit 130
