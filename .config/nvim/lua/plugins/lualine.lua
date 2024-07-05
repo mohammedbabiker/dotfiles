8157309f6d2b834e4958082c0685c8a816552a5c
Mohammed Babiker <mobabikerbabai20@gmail.com>
2024-07-05 21:11:11 +0300

    docker lsp and updates

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -11,13 +11,13 @@ return {
       sections = {
         lualine_a = { "filename" },
         lualine_b = { "branch", "diff", "diagnostics" },
-        lualine_c = {
-          {
-            require("noice").api.statusline.mode.get,
-            cond = require("noice").api.statusline.mode.has,
-          },
-          -- "mode",
-        },
+        -- lualine_c = {
+        --   {
+        --     require("noice").api.statusline.mode.get,
+        --     cond = require("noice").api.statusline.mode.has,
+        --   },
+        --   -- "mode",
+        -- },
         lualine_x = {
           -- "encoding",
           "fileformat",
