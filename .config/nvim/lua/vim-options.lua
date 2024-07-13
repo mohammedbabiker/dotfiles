81e537e0207ff8a28844d3d24f8f3545445a5089
Mohammed Babiker <mobabikerbabai20@gmail.com>
2024-07-13 17:44:31 +0300

    update netrew

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -26,6 +26,7 @@ vim.cmd("set conceallevel=2")
 vim.cmd("set inccommand=split")
 vim.cmd("set virtualedit=block")
 vim.cmd("set isfname+=@-@")
+vim.cmd("let g:netrw_banner = 0")
 
 -- general keymap
 
